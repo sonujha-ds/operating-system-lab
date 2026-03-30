63c320bf8d7202d03edb1a592b8bd42644f6c798
Sonu jha <sonuk020658@gmail.com>
2026-03-30 14:50:47 +0530

    Delete cpu-scheduling-os-lab directory

--- cpu-scheduling-os-lab/process.py
+++
@@ -1,8 +0,0 @@
-class Process:
-    def __init__(self, pid, at, bt):
-        self.pid = pid
-        self.at = at  # Arrival Time
-        self.bt = bt  # Burst Time
-        self.ct = 0   # Completion Time
-        self.tat = 0  # Turnaround Time
-        self.wt = 0   # Waiting Time
